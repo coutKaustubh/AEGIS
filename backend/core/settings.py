5f0c8ee4202b755258f1de62146b991292dbe835
KAUSTUBH SRIVASTAVA <kaustubhsrivastavajee22@gmail.com>
2026-09-04 23:22:06 +0530

    edited settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.users',
     'apps.chats',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
